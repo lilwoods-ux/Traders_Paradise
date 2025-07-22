5b55c1ba309b58ffc5ee08f526cd0ff148fe10df
lilwoods-ux <mdogotimothy05@gmail.com>
2025-07-22 16:10:57 +0300

    test one

--- Traders_Paradise/settings.py
+++ Traders_Paradise/settings.py
@@ -84,12 +84,14 @@ WSGI_APPLICATION = 'Traders_Paradise.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-    }
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'timothy23',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
+}
+
 }
 
 # Password validation
